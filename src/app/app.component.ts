72e1c6a618cc1e6a1e4565c9f0ea4d23fae3cb60
Lucas Castro <lukas.castro@live.com>
2020-10-08 22:53:46 -0300

    criado pj do loja com angular, bootstrap e jquery

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
+import $ from "jquery";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent  { 
   title = 'ProjetoLKSStore';
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TopoComponent } from './topo/topo.component';
+import { CentroComponent } from './centro/centro.component';
+import { RodapeComponent } from './rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopoComponent,
+    CentroComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule
